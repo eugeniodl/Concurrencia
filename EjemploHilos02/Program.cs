adba33de8dbb7f7f1770c9c16be48fd5e4f4be80
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-03-28 11:52:12 -0600

    Ejemplo de hilos

---
+++ EjemploHilos02/Program.cs
@@ -0,0 +1,34 @@
+﻿public class EjemploHilos02
+{
+
+    public static void Main()
+    {
+        // Instanciamos el objeto thread / hilo y
+        // colocamos el delegado que corresponde
+        Thread miHilo = new Thread(Mensaje);
+        miHilo.Start();
+
+        Thread miHilo2 = new Thread(MensajeConParametro);
+        miHilo2.Start(5);
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Mensaje desde Main");
+    }
+
+    // Creamos el método que se ejecutará en el segundo hilo
+    // Debe coincidir con el delegado
+    private static void Mensaje(object? obj)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Mensaje desde el segundo hilo");
+    }
+
+    // Este método se ejecuta en el segundo hilo
+    // y recibe un parámetro del exterior
+    private static void MensajeConParametro(object? obj)
+    {
+        int p = Convert.ToInt32(obj);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Mensaje con parámetro {0} {1}", p, p * 2);
+    }
+}
---
+++ EjemploHilos03/Program.cs
@@ -0,0 +1,34 @@
+﻿// Mostramos cómo ejecutar múltiples hilos
+public class EjemploHilos03
+{
+    private static bool ejecutar = true;
+    private static int n = 0;
+
+    public static void Main()
+    {
+        int m = 0;
+        // Aquí estamos creando múltiples hilos
+        for(int i = 0; i < 8; i++)
+        {
+            Thread miHilo = new Thread(Mensaje);
+            miHilo.Start(i);
+        }
+
+        while(ejecutar)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Mensaje desde el Main {0}", m);
+            m++;
+
+            if(m == 2500)
+                ejecutar = false;
+        }
+    }
+
+    private static void Mensaje(object? obj)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Mensaje desde el hilo {0} - {1}", obj, n);
+        n++;
+    }
+}
