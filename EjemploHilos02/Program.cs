821bfa05b17479d2cf36b51304a6ea618f22a965
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-03-28 09:55:51 -0600

    Ejemplos de hilos

---
+++ EjemploHilos02/Program.cs
@@ -0,0 +1,32 @@
+﻿public class EjemploHilos02
+{
+    public static void Main()
+    {
+        // Instanciamos el objeto thread / hilo y
+        // colocamos el delegado que corresponde
+        Thread hilo1 = new Thread(Mensaje);
+        hilo1.Start();
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Mensaje desde Main");
+
+        Thread hilo2 = new Thread(MensajeConParametro);
+        hilo2.Start(6);
+    }
+
+    // Creamos el método que se ejecutará en el segundo hilo
+    // Debe cincidir con el delegado
+    private static void Mensaje(object? obj)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Mensaje desde el hilo");
+    }
+
+    // Este método se ejecuta en el segundo hilo
+    // y recibe un parámetro del exterior
+    private static void MensajeConParametro(object? obj)
+    {
+        int p = Convert.ToInt32(obj);
+        Console.WriteLine("Mensaje con parámetro {0}, {1}", p, p * 3);
+    }
+}
---
+++ EjemploHilos04/Program.cs
@@ -0,0 +1,48 @@
+﻿// Foreground threads son hilos que siguen existiendo, aun si
+// la aplicación termina de ejecutar el Main
+// Pueden evitar que la aplicación termine
+
+// Background threads son hilos que se finalizan si la aplicación
+// termina de ejecutar el Main
+public class EjemploHilos04
+{
+    private static bool ejecutar = true;
+
+    public static void Main()
+    {
+        int m = 0;
+        // De esta forma creamos un hilo que es
+        // background thread
+        Thread miHilo = new Thread(new ThreadStart(Mensaje));
+        miHilo.IsBackground = true;
+        miHilo.Start();
+
+        while(ejecutar)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Mensaje desde el hilo principal {0}", m);
+            m++;
+
+            if(m == 25)
+                ejecutar = false;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Salimos del Main");
+    }
+
+    private static void Mensaje()
+    {
+        int n = 0;
+        bool ejecutarMensaje = true;
+        while(ejecutarMensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Mensaje desde el hilo {0}", n);
+            n++;
+
+            if(n > 25000)
+                ejecutarMensaje=false;
+        }
+    }
+}
