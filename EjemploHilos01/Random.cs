69b78913930b9a5732f3cb1cef445892f2e31c81
StephTeno <106124803+StephTeno@users.noreply.github.com>
2023-03-29 08:32:45 -0600

    Nueva Clase

---
+++ EjemploHilos01/Random.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploHilos01
+{
+    internal class Random
+    {
+    }
+}
