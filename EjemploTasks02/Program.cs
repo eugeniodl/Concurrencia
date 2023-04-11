776c3c8b09958e77e59f925cde8cdaa12fd4ecdc
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-04-11 10:21:16 -0600

    Ejemplos de Tasks

---
+++ EjemploTasks02/Program.cs
@@ -0,0 +1,16 @@
+﻿// La tarea anterior. También se puede crear con Task.Factory.StartNew.
+Task<DayOfWeek> taskA = new Task<DayOfWeek>(() => DateTime.Today.DayOfWeek);
+
+// La continuación. El delegado toma la tarea anterior
+// como argumento y puede devolver un tipo diferente
+Task<string> continuation = taskA.ContinueWith((antecedent) =>
+{
+    return String.Format("Hoy es {0}.", antecedent.Result);
+});
+
+// Iniciar la tarea anterior
+taskA.Start();
+
+// Utilice el resultado de la continuación
+Console.WriteLine(continuation.Result);
+Console.ReadKey();
