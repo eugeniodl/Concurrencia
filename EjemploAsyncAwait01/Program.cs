1f0354a42c7cfc3bb4b4f8a8f99042306e9e810b
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-04-12 15:57:54 -0600

    EjemploAsyncAwait01

--- EjemploAsyncAwait01/Program.cs
+++ EjemploAsyncAwait01/Program.cs
@@ -1,11 +1,23 @@
 ﻿Task t = Task.Run(() =>
 {
-    Thread.Sleep(5000);
+    //Thread.Sleep(5000);
+    Task.Delay(5000);
     Console.WriteLine("Ejecutando task");
 });
+await t;
 
-double resultado = LeerDataDesdeBD();
+//double resultado = LeerDataDesdeBD();
+double resultado = await LeerDataDesdeBDAsync();
 Console.WriteLine(resultado);
+
+async Task<double> LeerDataDesdeBDAsync()
+{
+    var t = new Task<double>(() => HacerCalculosIntensivos());
+    t.Start();
+    double resultado = await t;
+    return resultado;
+}
+
 
 double LeerDataDesdeBD()
 {
