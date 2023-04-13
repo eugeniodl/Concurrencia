73d2a6e1e42d6de5b74638a947409d01394c1549
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-04-13 08:41:15 -0600

    Ejemplos Parallel

---
+++ EjemploPlinq01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+var numeros = Enumerable.Range(1, 10000);
+var sw = new Stopwatch();
+sw.Start();
+
+var numerosFiltrados = (from num in numeros
+                        .AsParallel()
+                        .AsOrdered()
+                        where num % 2 == 0
+                        select num).ToList();
+
+Console.WriteLine("{0} even numbers out of {1} total",
+                  numerosFiltrados.Count(), numeros.Count());
+
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
---
+++ EjemploPlinq02/Program.cs
@@ -0,0 +1,36 @@
+﻿List<string> lista = new List<string>();
+int i = 0;
+
+while(i <= 1000)
+{
+    lista.Add(Cadena());
+    i++;
+}
+
+List<string> filtro = (from n in lista.AsParallel()
+                       where ContieneLetras(n)
+                       select n).ToList();
+
+foreach(var s in filtro)
+    Console.WriteLine(s);
+
+bool ContieneLetras(string cadena)
+{
+    bool resultado = false;
+    if(cadena.Contains("W"))
+        resultado = true;
+    return resultado;
+}
+
+string Cadena()
+{
+    var caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmenopqrstuvwxyz0123456789";
+    var array = new char[10];
+    var random = new Random();
+
+    for(int i = 0; i < array.Length; i++)
+        array[i] = caracteres[random.Next(caracteres.Length)];
+
+    var resultado = new String(array);
+    return resultado;
+}
