dce2fc782d58c8cd417393218536ef518e56cb6b
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-04-18 10:47:32 -0600

    EjemploPlinq03

---
+++ EjemploPlinq03/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] nombres = { "Jafet", "Axel", "Antony", "Alex", "Allan", "Davis", "Jean"};
+
+try
+{
+    var filtro = nombres.Where(a => a[0] == 'A');
+    var par = nombres.AsParallel().Where(a => a[0] == 'A');
+    Imprime(filtro);
+    Imprime(par);
+}
+catch (AggregateException e)
+{
+    foreach (var ex in e.InnerExceptions)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+void Imprime(IEnumerable<string> collection)
+{
+    Console.WriteLine("----------------");
+    foreach (var item in collection)
+    {
+        Console.WriteLine(item);
+    }
+}
+
